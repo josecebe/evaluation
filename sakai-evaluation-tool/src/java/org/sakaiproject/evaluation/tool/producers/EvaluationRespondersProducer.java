65ad82ab49f86fd6fa6424833075f7ced9d1f30e
josecebe <josecebe@gmail.com>
2017-10-24 11:45:41 +0200

    EVALSYS-1533 - Create a new role to see the responders status

--- sakai-evaluation-tool/src/java/org/sakaiproject/evaluation/tool/producers/EvaluationRespondersProducer.java
+++ sakai-evaluation-tool/src/java/org/sakaiproject/evaluation/tool/producers/EvaluationRespondersProducer.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.sakaiproject.evaluation.constant.EvalConstants;
 import org.sakaiproject.evaluation.logic.EvalCommonLogic;
@@ -115,6 +116,11 @@ public class EvaluationRespondersProducer extends EvalCommonProducer implements 
         boolean controlEval = evaluationService.canControlEvaluation(currentUserId, evaluationId);
 
         boolean allowEmailStudents = (Boolean) settings.get(EvalSettings.INSTRUCTOR_ALLOWED_EMAIL_STUDENTS);
+        List<EvalGroup> allowedGroups = commonLogic.getEvalGroupsForUser(currentUserId, EvalConstants.PERM_VIEW_RESPONDERS);
+        List<String> allowedGroupIds = new ArrayList<>();
+        for (EvalGroup allowedGroup : allowedGroups) {
+            allowedGroupIds.add(allowedGroup.evalGroupId);
+        }
         boolean allowViewResponders = (Boolean) settings.get(EvalSettings.INSTRUCTOR_ALLOWED_VIEW_RESPONDERS);
         int responsesRequired = ((Integer) settings.get(EvalSettings.RESPONSES_REQUIRED_TO_VIEW_RESULTS));
         boolean currentUserViewResponses = controlEval || allowViewResponders;
@@ -138,7 +144,7 @@ public class EvaluationRespondersProducer extends EvalCommonProducer implements 
             String userId = response.getOwner();
             groupToUserResponses.get(groupId).put(userId, response);
         }
-        boolean showStatus = ((responses.size() >= responsesRequired) || currentUserViewResponses);
+        boolean showStatus = (responses.size() >= responsesRequired) && currentUserViewResponses;
 
         String statsAssigned;
         if (evalAnonymous) {
@@ -191,6 +197,10 @@ public class EvaluationRespondersProducer extends EvalCommonProducer implements 
         for (Entry<String, List<EvalUser>> entry : usersByGroupId.entrySet()) {
             UIBranchContainer groupBranch = UIBranchContainer.make(tofill, "responseGroups:");
             String groupId = entry.getKey();
+            boolean showEntryStatus = false;
+            if (!allowedGroupIds.isEmpty() && allowedGroupIds.contains(groupId)) {
+                showEntryStatus = true;    
+            }
             Map<String, EvalResponse> userResponses = groupToUserResponses.get(groupId);
             if (userResponses == null) {
                 userResponses = new HashMap<>(0);
@@ -215,7 +225,7 @@ public class EvaluationRespondersProducer extends EvalCommonProducer implements 
             for (EvalUser evalUser : users) {
                 UIBranchContainer userResponseBranch = UIBranchContainer.make(showResponsesBranch, "responses:");
                 UIOutput.make(userResponseBranch, "responseUser", evalUser.displayName + "(" + evalUser.username + ")");
-                if (showStatus) {
+                if (showStatus && showEntryStatus) {
                     String messagekey = "evalresponders.status.untaken"; // untaken (no response)
                     EvalResponse response = userResponses.get(evalUser.userId);
                     if (response != null) {
@@ -226,7 +236,7 @@ public class EvaluationRespondersProducer extends EvalCommonProducer implements 
                         }
                     }
                     UIMessage.make(userResponseBranch, "responseStatus", messagekey);
-                } else if (currentUserViewResponses) {
+                } else if (currentUserViewResponses && responsesRequired > 0) {
                     // user can view but not enough responses yet
                     UIMessage.make(userResponseBranch, "responseStatus", "controlevaluations.eval.report.after.responses", 
                             new Object[] {responsesRequired});
